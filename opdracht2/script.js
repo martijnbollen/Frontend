d057583c1ca9167157d4d7b681d401ffcfedc4ea
Martijn Bollen <mta.bollen94@gmail.com>
2019-03-13 11:37:55 +0100

    tweede interactie toegevoegd

--- opdracht2/script.js
+++ opdracht2/script.js
@@ -1,8 +1,10 @@
 var indicator = 1;
 var display = document.getElementById('center');
-var linksknoppen = document.querySelectorAll('button:first-of-type');
-var rechtsknoppen = document.querySelectorAll('button:nth-of-type(2)');
+var linksknoppen = document.querySelectorAll('.centerpiece button:first-of-type');
+var rechtsknoppen = document.querySelectorAll('.centerpiece button:nth-of-type(2)');
 var div = document.querySelector('div');
+var linksskippen = document.querySelector('.bar button:first-of-type');
+var rechtsskippen = document.querySelector('.bar button:nth-of-type(2)');
 
 
 
@@ -22,20 +24,6 @@ linksknoppen.forEach(function(linksknop) {
     })
 });
 
-div.addEventListener("scroll", function () {
-        if (indicator === 1) {
-            console.log("overflow");
-            document.querySelector('.bar img:nth-of-type(' + indicator + ')').classList.add = 'active';
-        }
-        else {
-            indicator--;
-            console.log(indicator);
-            display.src = "img/foto" +indicator+ ".jpg";
-            document.querySelector('.bar img:nth-of-type(' + indicator + ')').classList.add = 'active';
-        }
-
-});
-
 rechtsknoppen.forEach(function(rechtsknop) {
     rechtsknop.addEventListener("click", function () {
         if (indicator == 4) {
@@ -51,16 +39,31 @@ rechtsknoppen.forEach(function(rechtsknop) {
 
     })
 });
-div.addEventListener("scroll", function () {
-        if (indicator == 4) {
-            console.log("overflow");
-            document.querySelector('.bar img:nth-of-type(' + indicator + ')').classList.add = 'active';
+
+linksskippen.addEventListener("dblclick", function () {
+        if (indicator != 1) {
+            var indicator = 1;
+            console.log('linksskip');
+            display.src = "img/foto" +indicator+ ".jpg";
         }
         else {
-            indicator++;
-            console.log(indicator);
             display.src = "img/foto" +indicator+ ".jpg";
-            document.querySelector('.bar img:nth-of-type(' + indicator + ')').classList.add = 'active';
         }
+
+
+
 });
 
+rechtsskippen.addEventListener("dblclick", function () {
+        if (indicator != 4) {
+            var indicator = 4;
+            console.log('rechtsskip');
+            display.src = "img/foto" +indicator+ ".jpg";
+        }
+        else {
+            display.src = "img/foto" +indicator+ ".jpg";
+        }
+
+
+
+});
