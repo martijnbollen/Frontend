f9a988c1e72d4cd1c32a056d1be03c26f720cae3
Martijn Bollen <mta.bollen94@gmail.com>
2019-04-10 09:38:56 +0200

    Update script.js

--- opdracht3/tutorial volgen/script/script.js
+++ opdracht3/tutorial volgen/script/script.js
@@ -21,56 +21,56 @@ request.onload = function () {
     invullenHeader(weer);
 };
 
-function invullenHeader(jsonObj) {
-    var myH1 = document.createElement('h1');
-    var weather = jsonObj["weather"];
-    header.appendChild(myH1);
-    console.log(weather);
-    var myH3 = document.createElement('h3');
-    var myPara1 = document.createElement('p');
-    var myPara2 = document.createElement('p');
-    var wind = jsonObj["wind"];
-    myH3.textContent ='Weersomstandigheden:';
-    aside.appendChild(myH3);
-    myPara1.textContent = 'Windkracht: ' + wind.speed;
-    aside.appendChild(myPara1);
-    var windrichting = document.createElement('img');
-    windrichting.src = '../img/kompas.png';
-    var richting = wind.deg;
-    console.log(wind.speed);
-    var windkracht = wind.speed;
-    var main = jsonObj["main"];
-    var temperatuur = main.temp;
-    console.log(temperatuur);
-    myPara2.textContent = 'Temperatuur: ' + temperatuur + ' °C';
-    aside.appendChild(myPara2);
+    function invullenHeader(jsonObj) {
+        var myH1 = document.createElement('h1');
+        var weather = jsonObj["weather"];
+        header.appendChild(myH1);
+        console.log(weather);
+        var myH3 = document.createElement('h3');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var wind = jsonObj["wind"];
+        myH3.textContent ='Weersomstandigheden:';
+        aside.appendChild(myH3);
+        myPara1.textContent = 'Windkracht: ' + wind.speed;
+        aside.appendChild(myPara1);
+        var windrichting = document.createElement('img');
+        windrichting.src = '../img/kompas.png';
+        var richting = wind.deg;
+        console.log(wind.speed);
+        var windkracht = wind.speed;
+        var main = jsonObj["main"];
+        var temperatuur = main.temp;
+        console.log(temperatuur);
+        myPara2.textContent = 'Temperatuur: ' + temperatuur + ' °C';
+        aside.appendChild(myPara2);
 
-    knoppen.forEach(function(knop) {
-    knop.addEventListener("click", function() {
-        var cityID = this.getAttribute('value');
-        afbeelding.style.width = '5em';
-        artikel.style.visibility = 'hidden';
-//        windrichting.style.transform = 'rotate('+ richting + 'deg)';
-        aside.style.position = 'relative';
-        aside.style.top = '-2em';
-        knop.style.visibility = 'hidden';
-        myH1.textContent = weather[0].description;
-        aside.style.visibility = 'visible';
-        if (windkracht >= 5 && temperatuur >= 10) {
-            myH2.textContent = 'Ja! Je kan vandaag gaan kitesurfen. Het is lekker warm en er staat een goede wind.';
-            afbeelding.src = 'img/kitesurf.gif';
-        }
-         else if (windkracht < 5 && temperatuur >= 10) {
-            myH2.textContent = 'Hmm... Ik weet het niet zeker. Het is lekker weer, maar ik weet niet zeker of er genoeg wind staat.';
-            afbeelding.src = 'img/nokitesurf.gif';
+            afbeelding.style.width = '5em';
+            artikel.style.visibility = 'hidden';
+    //        windrichting.style.transform = 'rotate('+ richting + 'deg)';
+            aside.style.position = 'relative';
+            aside.style.top = '-2em';
+            knop.style.visibility = 'hidden';
+            myH1.textContent = weather[0].description;
+            aside.style.visibility = 'visible';
+            if (windkracht >= 5 && temperatuur >= 10) {
+                myH2.textContent = 'Ja! Je kan vandaag gaan kitesurfen. Het is lekker warm en er staat een goede wind.';
+                afbeelding.src = 'img/kitesurf.gif';
+            }
+             else if (windkracht < 5 && temperatuur >= 10) {
+                myH2.textContent = 'Hmm... Ik weet het niet zeker. Het is lekker weer, maar ik weet niet zeker of er genoeg wind staat.';
+                afbeelding.src = 'img/nokitesurf.gif';
 
-        }
-         else if (windkracht >= 5 && temperatuur < 10) {
-            myH2.textContent = 'Hmmm... Ik weet het niet zeker. Er is genoeg wind om te surfen, maar het weer is niet top.';
+            }
+             else if (windkracht >= 5 && temperatuur < 10) {
+                myH2.textContent = 'Hmmm... Ik weet het niet zeker. Er is genoeg wind om te surfen, maar het weer is niet top.';
 
-        }
-         else {
-            myH2.textContent = 'Nee, helaas. Het weer is niet goed en er staat niet genoeg wind om te surfen.';
-            afbeelding.src = 'img/nokitesurf.gif';
-        }
-    })})};
+            }
+             else {
+                myH2.textContent = 'Nee, helaas. Het weer is niet goed en er staat niet genoeg wind om te surfen.';
+                afbeelding.src = 'img/nokitesurf.gif';
+            }
+        };
+    })
+});
+
